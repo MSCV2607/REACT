53f6119817e2573faad250ef189e260ee3a991f3
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-08-10 18:14:56 -0300

    Scope

--- 02-IntroJS/src/main.jsx
+++ 02-IntroJS/src/main.jsx
@@ -1,1 +1,16 @@
-console.log('Hello, World!');
+
+
+//Se trabajan con Variables y constantes
+const nombre = "Fernando";
+const apellido = "Herrera";
+
+let valorDado = 5;
+
+console.log(nombre,apellido,valorDado);
+
+//Se trabaja con el scope de las variables asi se puede poner una variable ya inicializada
+if (true) {
+  const nombre = "Peter";
+  let valorDado = 6;
+  console.log(valorDado);
+}
