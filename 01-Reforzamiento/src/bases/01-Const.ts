f2e52929dd4eb21f429862f9615168e09eca5fbd
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-08-19 17:30:06 -0300

    VAriable y constantes

---
+++ 01-Reforzamiento/src/bases/01-Const.ts
@@ -0,0 +1,12 @@
+//Variables con ts usamos let para declarar variables o una constante const
+
+const firstName = 'John';
+const lastName = 'Doe';
+
+let numeroDado = 5;
+
+console.log(firstName);
+console.log(lastName);
+
+const contatainLetterH = lastName.includes('H');    
+console.log({contatainLetterH, numeroDado});
---
+++ 01-Reforzamiento/src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import "./bases/01-Const";
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+   <h1> HI </h1>
+  </div>
+`
+
