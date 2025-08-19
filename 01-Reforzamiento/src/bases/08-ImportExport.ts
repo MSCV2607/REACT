76d1eaebb5641f44d5559d1aeabd750caf4ad7f5
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-08-19 19:25:21 -0300

    Import y Export

---
+++ 01-Reforzamiento/src/bases/08-ImportExport.ts
@@ -0,0 +1,10 @@
+import { heroes, type Hero } from "../data/heros.data"
+
+const getHeroById = (id:number): Hero | undefined => {
+    const hero = heroes.find((hero) => {
+        return hero.id === id;
+    })
+    return hero;
+}
+
+console.log(getHeroById(2));
--- 01-Reforzamiento/src/main.ts
+++ 01-Reforzamiento/src/main.ts
@@ -5,6 +5,8 @@ import "./bases/01-Const";
 import "./bases/02-TemplateString";
 import "./bases/03-ObjectLiteral";
 import "./bases/04-Arreglo";
+import "./bases/05-Funciones";
+import "./bases/08-ImportExport"
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
    <h1> HI </h1>
