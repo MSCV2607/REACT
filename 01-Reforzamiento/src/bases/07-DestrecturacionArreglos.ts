7187367df5ac7fc0865a018eb80cd850eab994d2
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-08-19 18:45:51 -0300

    Tarea

--- 01-Reforzamiento/src/bases/07-DestrecturacionArreglos.ts
+++ 01-Reforzamiento/src/bases/07-DestrecturacionArreglos.ts
@@ -15,3 +15,19 @@ const returnsArrayFn = () => {
 
 const [letters, numbers] = returnsArrayFn();
 console.log(letters, numbers);
+
+
+//Tarea
+
+const useState = (value:string) => {
+    return [
+        value,
+        (newValue:string) => {
+            console.log(newValue)
+        },
+    ] as const;
+}
+
+const [name, setName] = useState('Goku');
+console.log(name);       // Goku
+setName('Vegeta');       // Imprime "Vegeta"
