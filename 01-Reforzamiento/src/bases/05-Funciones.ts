06c7e5a48896eea5a3296f8f868a167e7c355fc9
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-08-19 18:20:27 -0300

    Funcion con multiples retornos

--- 01-Reforzamiento/src/bases/05-Funciones.ts
+++ 01-Reforzamiento/src/bases/05-Funciones.ts
@@ -1,17 +1,15 @@
 
 
 //Funcion que espera un string y devuelve un string
-function greet(name: string): string {
+function saludo(name: string): string {
   return `Hello, ${name}!`;
 }
 
-//Funciones de flecha
-const saludo2 = (name: string) => {
-    return `Hola, ${name}!`;
-}
+//Funciones de flecha mas resumido
+const saludo2 = (name: string) => `Hello, ${name}!`;
 
 //Invoco la funcion con un argumento
-const message = greet("Goku");
+const message = saludo("Goku");
 
 const message2 = saludo2("Vegetta");
     
@@ -20,7 +18,7 @@ console.log(message2);
 
 
 
-
+//Funcion que devuelve un objeto
 function getUser(){
     return {
         id: 1,
@@ -29,12 +27,22 @@ function getUser(){
     
 }
 
-const getUser2 = () => {
-    return {
+//Funcion de flecha que devuelve un objeto
+const getUser2 = () => ({
         id: 2,
         name: "Vegeta"
-    };
-}
+    });
 
 const user = getUser();
 console.log(user);
+
+const user2 = getUser2();
+console.log(user2);
+
+
+const misNumeros = [1, 2, 3, 4, 5];
+
+//Funcion que recorre un arreglo y muestra sus elementos
+misNumeros.forEach((numero) => {
+    console.log(numero);
+});
