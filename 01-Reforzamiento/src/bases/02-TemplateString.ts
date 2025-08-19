b430d649875515e04a8e6ca8fe5005d433c11db0
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-08-19 17:34:45 -0300

    Template String

---
+++ 01-Reforzamiento/src/bases/02-TemplateString.ts
@@ -0,0 +1,6 @@
+const firstName = 'John';
+const lastName = 'Doe';
+
+const fullName = `El nombre es ${firstName} y ${lastName}`
+
+console.log(fullName);
--- 01-Reforzamiento/src/main.ts
+++ 01-Reforzamiento/src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import "./bases/01-Const";
+import "./bases/02-TemplateString";
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
    <h1> HI </h1>
